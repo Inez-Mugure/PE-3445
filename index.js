1183b4d16681609f03e4a0f5929e74aa4796923d
Inez <100865888+Inez-Mugure@users.noreply.github.com>
2022-08-18 16:45:55 +0300

    Added logic to filter bill payments with amount > 0

--- index.js
+++ index.js
@@ -40,18 +40,24 @@ app.get('/api/bill_payments', async (req, res) => {
     res.json(JSON.parse(data));   
 })
 
+// Array#filter() so that only bill payments > 0 appear on the API
+const data = [
+    {
+    "id": "dd57c78e-7d9d-4f5d-ba6b-34587aeed680",
+    "created_at": "10 Aug, 2022, 7:11 AM",
+    "user_id": "40562a30-2b9d-11ea-b835-255a5f94df01",
+    "status": "FAILED",
+    "amount": 0,
+    "billder_id": "gabaxibox__cabletv__dstv",
+    "biller_name": "DSTV",
+    "biller_item_id": "gabaxibox__dstv__CTBSCAP0103",
+    "biller_item_name": "DStv Compact"
+    },
+];
+    
+const filterByAmount = () => data.filter(({ amount }) => amount >0);
+console.log("filterByAmount::",filterByAmount())
+
 // port
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
-
-
-
- /* 
- fs.readFile("./data.json", "utf8", (err, jsonString) => {
-     if (err) {
-         console.log("File read failed:", err);
-          return;
-        }
-        res.send(jsonString);
-      });
-*/
