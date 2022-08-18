7239713fb5c5b2921b53e091c4957d1a1a93ed25
Inez <100865888+Inez-Mugure@users.noreply.github.com>
2022-08-18 13:57:02 +0300

    Served JSON file to display given bill payments

---
+++ README.md
@@ -0,0 +1,6 @@
+<!-- 
+Notes
+npm install -D nodemon: installed nodemon as a developer dependency because my server code will not depend on it.
+alternative [ES6] syntax to import express
+import express from 'express'; 
+-->
---
+++ index.js
@@ -0,0 +1,57 @@
+/* 
+common js syntax
+importing packages
+ */
+
+// Requiring module
+const express = require('express');
+
+// reference the path of the data.json file
+const config = require("./data.json");
+
+const fs = require("fs");
+
+// Creating express app object 
+const app = express();
+
+// middlewares
+app.use(express.json());
+
+// defining a route
+app.get('/', (req, res) => {
+    res.send('Bill Payments')
+});
+
+// 
+const readFile = async filePath => {
+    try {
+      const data = await fs.promises.readFile(filePath, 'utf8')
+      console.log("jsonData", data)
+      return data
+    }
+    catch(err) {
+      console.log(err)
+    }
+  }
+
+// Interact with files with fs
+app.get('/api/bill_payments', async (req, res) => {  
+    const data = await readFile("./data.json")   
+    res.json(JSON.parse(data));   
+})
+
+// port
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on Port: ${port}`));
+
+
+
+ /* 
+ fs.readFile("./data.json", "utf8", (err, jsonString) => {
+     if (err) {
+         console.log("File read failed:", err);
+          return;
+        }
+        res.send(jsonString);
+      });
+*/
