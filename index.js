48127a18c5c371a7708645112bb3b431edb39ac1
Inez <100865888+Inez-Mugure@users.noreply.github.com>
2022-09-21 14:33:46 +0300

    successfully migrated from express to koa, all code running

--- index.js
+++ index.js
@@ -22,8 +22,35 @@ router.get('/', async (ctx) => {
   ctx.body = data_2
 });
 
+// defining an endpoint to return bills with amount greater than 0
+router.get('/bill_payments', async (ctx) => {  
+  const filterData = await data.bill_payments.filter((bill) => bill.amount > 0);
+  ctx.body = {filterData};   
+  console.log(filterData);
+})
+
+// defining an endpoint to return bills with amount from smallest to greatest
+router.get('/bill_payments/sorted-by-amount', (ctx) => {
+  data_2.bill_payments.sort((a, b) => {
+    return a.amount-b.amount
+    });
+  ctx.body = {data_2};
+})
+
+//defining an endpoint to return bills with property created at from earliest to latest
+router.get('/bill_payments/sorted-by-created-at', (ctx) => {
+  data_2.bill_payments.sort(
+    (a, b) =>
+      new Moment(a.created_at).format('X') -
+      new Moment(b.created_at).format('X')
+ );
+
+  console.log(data_2);
+  ctx.body = {data_2};
+})  
+
 app
   .use(router.routes())
   .use(router.allowedMethods());
-
+  
 app.listen(3000);
